4647ab98a4c24a1ec10f03939ec4a5257e82eddb
Gabor Kovacs <90977759+baroquemusic@users.noreply.github.com>
2021-09-18 20:41:41 -0700

    Update kalyha_config.php

--- include/kalyha_config.php
+++ include/kalyha_config.php
@@ -7,6 +7,6 @@ session_start();
 
 $kalyha -> session_user_id = $_SESSION['id_of_user'];
 
-$kalyha -> initDB('eagleaircraftch.ipagemysql.com', 'ignicad_com', 'nxYrAf0p2D', 'ignicad_com', 'felhasznalok', 'projektek', 'fustjaratok');
+$kalyha -> initDB('sql3.freesqldatabase.com', 'sql3438272', 'G6C7lXK5WM', 'sql3438272', 'felhasznalok', 'projektek', 'fustjaratok');
 
 ?>
